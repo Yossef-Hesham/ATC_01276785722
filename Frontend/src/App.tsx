6b67ded4ad927567c76e1889753121c048cf4030
youssef <youss4923@gmail.com>
2025-05-09 20:39:18 +0300

    fornt

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,10 @@
+
+// This file is needed for the project structure but we're using only HTML/CSS/JS
+// This is a minimal React component that doesn't do anything
+import React from 'react';
+
+const App = () => {
+  return <div></div>;
+};
+
+export default App;
