7a58bc6d86cc907e49de63b8eb6b3aab3acfb766
youssef <youss4923@gmail.com>
2025-05-14 19:45:18 +0300

    Delete frontend

--- Frontend/src/components/auth/index.ts
+++
@@ -1,7 +0,0 @@
-
-// Export all authentication components
-export { default as LoginForm } from './LoginForm';
-export { default as RegisterForm } from './RegisterForm';
-export { default as AuthLayout } from './AuthLayout';
-export { default as PasswordInput } from './PasswordInput';
-export { default as Authentication } from './Authentication';
