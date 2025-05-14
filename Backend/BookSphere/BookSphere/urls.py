ade70d9b3a7c029cf917e956fde9f71a974dbd52
youssef <youss4923@gmail.com>
2025-05-15 01:51:47 +0300

    first_deploy

--- Backend/BookSphere/BookSphere/urls.py
+++ Backend/BookSphere/BookSphere/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', admin.site.urls),
     path('api/', include('accounts.urls')),
     path('event/', include('events.urls')),
 ]
