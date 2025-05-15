5955f1186b6adae43c8c0593668f096e6de2382f
youssef <youss4923@gmail.com>
2025-05-16 01:54:37 +0300

    add doc

--- Backend/BookSphere/urls.py
+++ Backend/BookSphere/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('api/', include('accounts.urls')),
     path('event/', include('events.urls')),
     
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
