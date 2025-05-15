9e567ea7dc2c539c31b93f989089fbdd2a5b2058
youssef <youss4923@gmail.com>
2025-05-16 01:33:33 +0300

    add procfile

--- Backend/BookSphere/settings.py
+++ Backend/BookSphere/settings.py
@@ -34,7 +34,9 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.1', '']
+ALLOWED_HOSTS = ['localhost', '127.0.1', 'bookevent-production.up.railway.app']
+
+CSRF_TRUSTED_ORIGINS = ['https://bookevent-production.up.railway.app']
 
 # Application definition
 
