64ddcf3b4f8549e600c4ecaf87553330f0275649
youssef <youss4923@gmail.com>
2025-05-16 00:45:18 +0300

    deploy

--- Backend/BookSphere/BookSphere/wsgi.py
+++ Backend/BookSphere/BookSphere/wsgi.py
@@ -12,7 +12,4 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BookSphere.settings')
-
 application = get_wsgi_application()
-
-app = application
