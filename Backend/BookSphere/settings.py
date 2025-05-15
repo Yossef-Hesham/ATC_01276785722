e509caf9591cc8571ea222158c7d90bc8d3bd642
youssef <youss4923@gmail.com>
2025-05-16 01:24:25 +0300

    install package

--- Backend/BookSphere/settings.py
+++ Backend/BookSphere/settings.py
@@ -34,7 +34,7 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.1', '']
 
 # Application definition
 
